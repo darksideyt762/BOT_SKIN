fbc50b63985ed70f3feb59e19127b1313586b4ad
darksideyt762 <darksidegaming762@gmail.com>
2025-02-12 08:46:11 +0530

    Add files via upload

---
+++ unpack/00073730.lua
@@ -0,0 +1,32 @@
+-- !!! DO NOT MODIFY THIS FILE. !!!
+-- AUTO GENERATE BY pubgm_patch.py 2025-01-14 21:12:33
+global_patch_make_time = "" ..
+"Patch Version : 3.6.0.19560\n" ..
+"Mode : TAPD\n" ..
+"Branch : Release3.6.0\n" ..
+"Git Engine : 02dede90b\n" ..
+"Git Survive : 8ed8d16820\n" ..
+"Git Lua : d6c04100494\n" ..
+"SVN Xls : 1101821\n" ..
+"P4 Content : 506846\n" ..
+"BuildNo : #1\n" ..
+"HostName : v_heyzheng-PCm\n" ..
+"Mfg. Date : 2025-01-14 21:12:33\n" ..
+"Pipeline : #p-e8d1a2a90e7c42eda033a02ea5bb8f46\n"
+
+global_patch_make_time_map = {
+    ["Patch Version"] = "3.6.0.19560",
+    ["Mode"] = "TAPD",
+    ["Branch"] = "Release3.6.0",
+    ["Git Engine"] = "02dede90b",
+    ["Git Survive"] = "8ed8d16820",
+    ["Git Lua"] = "d6c04100494",
+    ["SVN Xls"] = "1101821",
+    ["P4 Content"] = "506846",
+    ["Pipeline"] =  "p-e8d1a2a90e7c42eda033a02ea5bb8f46",
+    ["BuildNo"] =  "#1",
+    ["HostName"] = "v_heyzheng-PCm",
+    ["Mfg. Date"] = "2025-01-14 21:12:33",
+}
+
+global_patch_tss = "hVfGkxIfp4dsL9J1ZhoHwA=="
